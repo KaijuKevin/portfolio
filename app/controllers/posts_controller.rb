2b3ed5f577ed19753fd75ff7bef7547904f8293d
Kevin <kevc.dev@gmail.com>
2015-10-05 14:35:46 -0700

    Add aside to pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	load_and_authorize_resource
-	
+
 	def index
 		@posts = Post.all
 	end
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
-	
+
 	def create
 		@post = Post.new(post_params)
 		if @post.save
