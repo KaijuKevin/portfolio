61d4b1940b0c4d258e9e626efee4fa26c97ebac4
Kevin <kevc.dev@gmail.com>
2015-10-05 16:03:26 -0700

    Change contact navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def octicon(code)
+    content_tag :span, '', :class => "octicon octicon-#{code.to_s.dasherize}"
+  end
+
+  def glyphicon(code)
+    content_tag :span, '', :class => "glyphicon glyphicon-#{code.to_s.dasherize}"
+  end
 end
